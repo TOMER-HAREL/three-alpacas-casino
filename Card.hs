a90ed8a6b5cb13eb31a74390b52540c78f3ad1fd
Victor Ingman <viregistermail@gmail.com>
2015-02-23 14:23:07 +0100

    Added show for PlayingCard, Suit and Value

--- Card.hs
+++ Card.hs
@@ -1,3 +1,19 @@
+instance Show Suit where
+    show Clubs = "♣︎"
+    show Diamonds = "♦︎"
+    show Hearts = "❤︎"
+    show Spades = "♠︎"
+
+instance Show Value where
+    show (Other value) = show(value)
+    show J = "Jack"
+    show Q = "Queen"
+    show K = "King"
+    show A = "Ace"
+
+instance Show PlayingCard where
+    show (Card suit value) = show(suit) ++ " " ++ show(value)
+
 data Suit = Clubs
           | Spades
           | Hearts
