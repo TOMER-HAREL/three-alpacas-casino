608fde0e46c9e0327a5071c5f13c35346183b14c
Victor Ingman <viregistermail@gmail.com>
2015-02-24 14:29:43 +0100

    Next broken build

--- GBlackJack.hs
+++ GBlackJack.hs
@@ -3,18 +3,29 @@ module GBlackJack where
   import Game
   import Card
 
-  {-
-    PURPOSE: Make the card more generic, that would be different values for
-      different games.
-    EXAMPLES:
-      value (Card Diamonds A None) == 13
-      value (Card Diamonds A BJ) == 11
-  -}
-  class Card a => BlackJack a where
-    value :: a -> Int
+  BJCardValue :: PlayingCard -> Int
+  BJCardValue (Card _ (Other value)) = value
+  BJCardValue (Card _ A) = 11
+  BJCardValue (Card _ _) = 10
 
-  instance BlackJack PlayingCard where
-    -- Black Jack
-    value (Card _ (Other value)) = value
-    value (Card _ A) = 11
-    value (Card _ _) = 10 --the rest of the cards
+  BJCalculateHand :: Hand -> Int
+  BJCalculateHand (Hand []) = 0
+  BJCalculateHand (Hand ((Card _ value):rest)) = (BJCardValue value) + BJCalculateHand rest
+
+  -- {-
+  --   PURPOSE: Make the card more generic, that would be different values for
+  --     different games.
+  --   EXAMPLES:
+  --     value (Card Diamonds A None) == 13
+  --     value (Card Diamonds A BJ) == 11
+  -- -}
+  -- class Card a => BlackJack a where
+  --   value :: a -> Int
+  --
+  -- data BJPlayingCard
+  --
+  -- instance Card FlexibleInstances => BlackJack PlayingCard where
+  --   -- Black Jack
+  --   value (Card _ (Other value)) = value
+  --   value (Card _ A) = 11
+  --   value (Card _ _) = 10 --the rest of the cards
