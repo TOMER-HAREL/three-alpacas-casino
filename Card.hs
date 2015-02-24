37bb403393d4f70056fb10fc08a51d62c7a2f20d
Linus Frosteryd <linus.frosteryd@gmail.com>
2015-02-24 12:04:43 +0100

    här är din
    victors som funkar

--- Card.hs
+++ Card.hs
@@ -4,31 +4,31 @@ module Card (PlayingCard(..)) where
   import Game
 
   {-
-    PURPOSE: to remove the hardcoded suitss of a card as we're designing for
+    PURPOSE: to remove the hardcoded values of a card as we're designing for
              multiple card games. Creating a class let's us design our own
              functions for a certain card.
   -}
   class Card a where
-    suits :: a -> Int
+    value :: a -> Int
 
   {-
-    PURPOSE: Make the card more generic, that would be different suitss for
+    PURPOSE: Make the card more generic, that would be different values for
       different games.
     EXAMPLES:
-      suits (Card Diamonds A None) == 13
-      suits (Card Diamonds A BJ) == 11
+      value (Card Diamonds A None) == 13
+      value (Card Diamonds A BJ) == 11
   -}
   instance Card PlayingCard where
     -- Black Jack
-    suits (Card _ (Other suits) BJ) = suits
-    suits (Card _ A BJ) = 11
-    suits (Card _ _ BJ) = 10 --the rest of the cards
+    value (Card _ (Other value) BJ) = value
+    value (Card _ A BJ) = 11
+    value (Card _ _ BJ) = 10 --the rest of the cards
     -- Default
-    suits (Card _ (Other suits) _) = suits
-    suits (Card _ J _) = 10
-    suits (Card _ Q _) = 11
-    suits (Card _ K _) = 12
-    suits (Card _ A _) = 13 --or 1?
+    value (Card _ (Other value) _) = value
+    value (Card _ J _) = 10
+    value (Card _ Q _) = 11
+    value (Card _ K _) = 12
+    value (Card _ A _) = 13 --or 1?
 
   -- Show
   instance Show Suit where
@@ -37,16 +37,16 @@ module Card (PlayingCard(..)) where
     show Hearts = "❤︎"
     show Spades = "♠︎"
 
-  instance Show suits where
-    show (Other suits) = show(suits)
+  instance Show Value where
+    show (Other value) = show(value)
     show J = "Jack"
     show Q = "Queen"
     show K = "King"
     show A = "Ace"
 
   instance Show PlayingCard where
-    show (Card suit suits None) = "[" ++ show(suit) ++ show(suits) ++ "]"
-    show (Card suit suits BJ) = "[BJ:" ++ show(suit) ++ show(suits) ++ "]"
+    show (Card suit value None) = "[" ++ show(suit) ++ show(value) ++ "]"
+    show (Card suit value BJ) = "[BJ:" ++ show(suit) ++ show(value) ++ "]"
 
   -- Eq
   instance Eq Suit where
@@ -57,10 +57,10 @@ module Card (PlayingCard(..)) where
     (==) _ _ = False
 
   instance Eq PlayingCard where
-    (==) c1@(Card s1 _ _) c2@(Card s2 _ _) = (suits c1 == suits c2) && (s1 == s2)
+    (==) c1@(Card s1 _ _) c2@(Card s2 _ _) = (value c1 == value c2) && (s1 == s2)
 
   instance Ord PlayingCard where
-    (<=) c1 c2 = (suits c1 <= suits c2)
+    (<=) c1 c2 = (value c1 <= value c2)
 
   -- data
   data Suit = Clubs
@@ -68,10 +68,10 @@ module Card (PlayingCard(..)) where
             | Hearts
             | Diamonds
 
-  data suits = Other Int 
+  data Value = Other Int
              | J
              | Q
-             | K
+             | K
              | A
 
-  data PlayingCard = Card Suit suits Game
+  data PlayingCard = Card Suit Value Game
