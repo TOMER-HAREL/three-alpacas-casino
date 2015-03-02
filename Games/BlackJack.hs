fd51d01fb886d0ea51b72c2616235b5dcd694f10
Victor Ingman <viregistermail@gmail.com>
2015-03-02 22:43:50 +0100

    Added two cases for calculating a hand with aces

--- Games/BlackJack.hs
+++ Games/BlackJack.hs
@@ -98,10 +98,10 @@ module Games.BlackJack where
 
   statesAvailable :: PlayingHand -> [PlayerState]
   statesAvailable (Hand (cards))
-                          | length (cards) == 2 && head (cards) /== last (cards) = [(State "DOUBLE"), (State "HIT"),(State "STAND")]
-                          | length (cards) == 2 && valueOf (head (cards)) == valueOf(last (cards)) = [(State "SPLIT"), (State "DOUBLE"), (State "HIT"),(State "STAND")]
-                          | length (cards) > 2 && 9 <= valueOf (Hand(cards)) && valueOf (Hand(cards)) <= 11 = [(State "DOUBLE"), (State "HIT"),(State "STAND")]
-                          | otherwise = [(State "HIT"),(State "STAND")]
+    | length (cards) == 2 && head cards /== last cards = [(State "DOUBLE"), (State "HIT"),(State "STAND")]
+    | length (cards) == 2 && valueOf (head cards) == valueOf(last (cards)) = [(State "SPLIT"), (State "DOUBLE"), (State "HIT"),(State "STAND")]
+    | length (cards) > 2 && 9 <= valueOf (Hand cards) && valueOf (Hand cards) <= 11 = [(State "DOUBLE"), (State "HIT"),(State "STAND")]
+    | otherwise = [(State "HIT"),(State "STAND")]
 
   {-
     PURPOSE: perform a move for one player.
@@ -114,16 +114,24 @@ module Games.BlackJack where
   performMove (Player hand role (State "STAND")) deck = [(Player hand role (UndefinedState))]
   performMove _ deck = undefined
 
-
-  testBJCalculateFatHand = T.TestCase $ T.assertBool "testFatHand" (valueOf (Hand [(Card Diamonds A), (Card Clubs A), (Card Spades (Other 7))]) == 0)
   {-
     TODO: Test cases
   -}
+  testBJCalculateFatHand = T.TestCase $ T.assertBool "testFatHand" (valueOf (Hand [(Card Diamonds K), (Card Clubs Q), (Card Spades (Other 3))]) == 23)
+  testBJCalculateAceHand = T.TestCase $ T.assertBool "testAceHand" (valueOf (Hand [(Card Diamonds A), (Card Clubs A)]) == 12)
+  testBJCalculateAce21 = T.TestCase $ T.assertBool "testAce21" (valueOf (Hand [(Card Diamonds A), (Card Clubs A), (Card Hearts (Other 9))]) == 21)
   testBJDrawCardFromDeck = T.TestCase $ T.assertBool "testBJDrawCardFromDeck" ((createEmptyDeck) == testDeck)
-  -- testBJperformMove = T.TestCase $ T.assertBool "testBJperformMove" ((performMove ) == testDeck)
   testBJstatesAvailable = T.TestCase $ T.assertBool "testBJstatesAvailable" (statesAvailable (Hand [(Card Diamonds (Other 3)), (Card Clubs (Other 5))]) == ([(State "DOUBLE"), (State "HIT"),(State "STAND")]))
   testBJstatesAvailable2 = T.TestCase $ T.assertBool "testBJstatesAvailable2" (statesAvailable (Hand [(Card Diamonds K), (Card Clubs K)]) == ([(State "SPLIT"), (State "DOUBLE"), (State "HIT"),(State "STAND")]))
   testBJstatesAvailable3 = T.TestCase $ T.assertBool "testBJstatesAvailable3" (statesAvailable (Hand [(Card Diamonds (Other 4)), (Card Clubs (Other 3)), (Card Hearts (Other 3))]) == ([(State "DOUBLE"), (State "HIT"),(State "STAND")]))
   testBJstatesAvailable4 = T.TestCase $ T.assertBool "testBJstatesAvailable4" (statesAvailable (Hand [(Card Diamonds (Other 7)), (Card Clubs (Other 3)), (Card Hearts (Other 3))]) == ([(State "HIT"),(State "STAND")]))
 
-  testListBJ = T.TestList [testCreateEmptyDeck, testDrawCardFromDeck, {-testBJperformMove,-} testBJstatesAvailable, testBJstatesAvailable2, testBJstatesAvailable3, testBJstatesAvailable4]
+  testListBJ = T.TestList [testCreateEmptyDeck,
+                          testDrawCardFromDeck,
+                          testBJstatesAvailable,
+                          testBJstatesAvailable2,
+                          testBJstatesAvailable3,
+                          testBJstatesAvailable4,
+                          testBJCalculateFatHand,
+                          testBJCalculateAceHand,
+                          testBJCalculateAce21]
